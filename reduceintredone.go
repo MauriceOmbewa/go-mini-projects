0b0bb4bdb145b02074041b283c825c7d89614cac
MAURICE ODHIAMBO OMBEWA <124437425+MauriceOmbewa@users.noreply.github.com>
2024-05-22 11:55:41 +0300

    added reducceintredone.go

---
+++ reduceintredone.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	mul := func(acc int, cur int) int {
+		return acc * cur
+	}
+	sum := func(acc int, cur int) int {
+		return acc + cur
+	}
+	div := func(acc int, cur int) int {
+		return acc / cur
+	}
+	as := []int{500, 2, 2}
+	ReduceInt(as, mul)
+	ReduceInt(as, sum)
+	ReduceInt(as, div)
+}
+
+func ReduceInt(a []int, f func(int, int) int) {
+	if len(a) == 0 {
+		return
+	}
+
+	acc := a[0]
+
+	for _, char := range a[1:] {
+		acc = f(acc, char)
+	}
+	fmt.Println(acc)
+}
