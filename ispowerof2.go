3f0a9bcf33e866cd49827d59c4ac290d223ce3b2
MAURICE ODHIAMBO OMBEWA <124437425+MauriceOmbewa@users.noreply.github.com>
2024-05-28 12:14:36 +0300

    added ispower.go

--- ispowerof2.go
+++ ispowerof2.go
@@ -1,31 +1,45 @@
 package main
 
 import (
-	"fmt"
-	"math"
 	"os"
 	"strconv"
+
+	"github.com/01-edu/z01"
 )
 
-func main() {
-	if len(os.Args) != 2 {
+func main(){
+	if len(os.Args) != 2{
 		return
 	}
 
-	first := os.Args[1]
+	input := os.Args[1]
+	inpconv, _:= strconv.Atoi(input)
 
-	firstconv, err := strconv.Atoi(first)
-	if err != nil {
-		fmt.Println("Error:", err)
+	var new bool
+
+	for inpconv > 0{
+		if inpconv%2 == 1{
+			new = false
+			break
+		} else if inpconv/ 2 == 1 {
+			new = true
+			break
+		}
+		inpconv = inpconv / 2
 	}
 
-	for i := 0; i < 99999999999; i++ {
-		if firstconv == int(math.Pow(2, float64(i))) {
-			fmt.Println("true")
-			return
-		} else {
-			fmt.Println("false")
-			return
-		}
+	if new == true{
+		z01.PrintRune('t')
+		z01.PrintRune('r')
+		z01.PrintRune('u')
+		z01.PrintRune('e')
+		z01.PrintRune('\n')
+	} else {
+		z01.PrintRune('f')
+		z01.PrintRune('a')
+		z01.PrintRune('l')
+		z01.PrintRune('s')
+		z01.PrintRune('e')
+		z01.PrintRune('\n')
 	}
 }
